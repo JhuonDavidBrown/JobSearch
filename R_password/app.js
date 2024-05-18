7a8a3302b1bd987cde53969a5a499922e45cabc9
Monica0317 <monicachicangana0317@gmail.com>
2024-05-18 17:11:35 -0500

    Update app.js

--- R_password/app.js
+++ R_password/app.js
@@ -1,10 +1,17 @@
 const open= document.getElementById('open');
 const modal_container=document.getElementById('modal_container');
 const close=document.getElementById('close');
+const modal = document.querySelector('.modal');
 
-open.addEventListener('click', () => {
-    modal_container.classList.add('show');
-})
 close.addEventListener('click', () => {
     modal_container.classList.remove('show');
-})
+    modal.classList.remove('modal-close');
+  });
+  
+  open.addEventListener('click', () => {
+    modal_container.classList.add('show');
+    modal.classList.add('modal-close');
+    setTimeout(() => {
+      modal.classList.remove('modal-close');
+    });
+  });
