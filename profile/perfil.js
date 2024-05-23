3ef857b360fcecf4c60a2db5c0c43a8676c76625
Monica Chicangana <monica.chicangana@uao.edu.co>
2024-05-22 23:38:00 -0500

    cambios en perfil y empleo pixel

--- profile/perfil.js
+++ profile/perfil.js
@@ -142,27 +142,40 @@ document.addEventListener('DOMContentLoaded', (event) => {
  const notiBtn = document.getElementById('notiBtn');
  const notiContainer = document.getElementById('container_n');
  
- // Función para alternar la visibilidad de la ventana de mensajes
- function toggleMsgContainer() {
-     if (msgContainer.style.display === 'none') {
-         msgContainer.style.display = 'block';
-         notiContainer.style.display = 'none'; // Cerrar la ventana de notificaciones
-     } else {
-         msgContainer.style.display = 'none';
-     }
- }
- 
- // Función para alternar la visibilidad de la ventana de notificaciones
- function toggleNotiContainer() {
-     if (notiContainer.style.display === 'none') {
-         notiContainer.style.display = 'block';
-         msgContainer.style.display = 'none'; // Cerrar la ventana de mensajes
-     } else {
-         notiContainer.style.display = 'none';
-     }
- }
- 
- // Agregar eventos de clic a los botones
- msgBtn.addEventListener('click', toggleMsgContainer);
- notiBtn.addEventListener('click', toggleNotiContainer);
+// Función para alternar la visibilidad de la ventana de mensajes
+function toggleMsgContainer() {
+    if (msgContainer.style.display === 'none') {
+        msgContainer.style.display = 'block';
+        notiContainer.style.display = 'none'; // Cerrar la ventana de notificaciones
+    } else {
+        msgContainer.style.display = 'none';
+    }
+}
+
+// Función para alternar la visibilidad de la ventana de notificaciones
+function toggleNotiContainer() {
+    if (notiContainer.style.display === 'none') {
+        notiContainer.style.display = 'block';
+        msgContainer.style.display = 'none'; // Cerrar la ventana de mensajes
+    } else {
+        notiContainer.style.display = 'none';
+    }
+}
+
+// Función para cerrar las ventanas emergentes cuando se hace clic fuera de ellas
+function closePopups(event) {
+    if (!msgContainer.contains(event.target) && !msgBtn.contains(event.target)) {
+        msgContainer.style.display = 'none';
+    }
+    if (!notiContainer.contains(event.target) && !notiBtn.contains(event.target)) {
+        notiContainer.style.display = 'none';
+    }
+}
+
+// Agregar eventos de clic a los botones
+msgBtn.addEventListener('click', toggleMsgContainer);
+notiBtn.addEventListener('click', toggleNotiContainer);
+
+// Agregar evento de clic al documento para cerrar las ventanas emergentes cuando se hace clic fuera de ellas
+document.addEventListener('click', closePopups);
 });
